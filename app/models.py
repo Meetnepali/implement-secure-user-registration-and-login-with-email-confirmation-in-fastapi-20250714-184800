d9e39983e55d721f95d38711d6bbf3b129b93026
Meetnepali <126581718+Meetnepali@users.noreply.github.com>
2025-07-14 18:48:45 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_confirmed = Column(Boolean, default=False)
+    confirmation_token = Column(String, nullable=True)
